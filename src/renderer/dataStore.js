8247faf5819f9e330e7d1989c955ea0815666f11
vdorchan <vdorchan@gmail.com>
2018-07-13 17:45:43 +0800

    fix:prevent the first time open the app,the db.json doesn't exist

--- src/renderer/dataStore.js
+++ src/renderer/dataStore.js
@@ -2,11 +2,22 @@ import low from 'lowdb'
 import FileSync from 'lowdb/adapters/FileSync'
 import path from 'path'
 import { app, remote } from 'electron'
+import fs from 'fs'
 
 const APP = process.type === 'renderer' ? remote.app : app
 
 const STORE_PATH = APP.getPath('userData')
 const STORE_FILE = path.join(STORE_PATH, '/db.json')
+
+// 不存在该文件时，先创建
+if (process.type !== 'renderer') {
+  try {
+    fs.accessSync(STORE_FILE)
+  } catch (error) {
+    fs.writeFileSync(STORE_FILE, '')
+  }
+}
+
 const adapter = new FileSync(STORE_FILE)
 const db = low(adapter)
 
